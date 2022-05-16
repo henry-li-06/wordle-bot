d64d6e711f9160751b8c5e779337029edc6525d1
Henry Li <hl738@cornell.edu>
2022-05-16 14:33:41 -0400

    untrack build and ds store

--- build/src/info-theory/GameEngine.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var GameEngine = /** @class */ (function () {
-    function GameEngine() {
-        this.numGuesses = 0;
-    }
-    return GameEngine;
-}());
-exports.default = GameEngine;
